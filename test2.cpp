e0d1830788596aaf723af56ed021caf4a53d6adc
Le Van Tuan Em <levantuanem00@gmail.com>
2025-07-13 22:08:17 +0700

    update code

--- test2.cpp
+++ test2.cpp
@@ -3,5 +3,15 @@
 int main() {
     int x = 100;
     printf("Value of x: %d\n", x);
+    float y = 3.14f;
+    printf("Value of y: %.2f\n", y);
+    char z = 'A';
+    printf("Value of z: %c\n", z);
+    printf("Hello, World!\n");
+    printf("Goodbye, World!\n");
+    printf("This is a C++17 program.\n");
+    printf("End of program.\n");
+    double pi = 3.14159;
+    printf("Value of pi: %.5f\n", pi);
     return 0;
 }
