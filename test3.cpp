2ab7cac55799aa171b8d53979e42c2177e273103
Le Van Tuan Em <levantuanem00@gmail.com>
2025-07-14 19:10:16 +0700

    code test vi tri ram

--- test3.cpp
+++ test3.cpp
@@ -4,5 +4,11 @@ int main() {
     unsigned char a = 100;
     printf("Gia tri cua a: %d\n", a);
     printf("Dia chi cua a: %d\n", &a); /*Địa chỉ của a trong RAM*/ 
+    int b = 200;
+    printf("Gia tri cua b: %d\n", b);
+    printf("Dia chi cua b: %d\n", &b); /*Địa chỉ của b trong RAM*/
+    float c = 3.14;
+    printf ("Gia tri cua c: %.2f\n", c);
+    printf("Dia chi cua c: %d\n", &c); /*Địa chỉ của c trong RAM*/
     return 0;
 }
